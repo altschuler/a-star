e97fb8aab7dec49608720998137a5d9cc84c0c68
patrickgadd <patrick_gadd@hotmail.com>
2013-05-05 11:32:00 +0200

    added a line to the report

--- WpfApplication1/Properties/Resources.Designer.cs
+++ WpfApplication1/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Heureka.RouteFinding.Properties {
+namespace ProjectAI.Heureka.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Heureka.RouteFinding.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ProjectAI.RouteFinding.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ProjectAI.Heureka.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
